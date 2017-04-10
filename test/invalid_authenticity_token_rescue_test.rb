f6b49f50beff1a5b8698c69df2e64ba7ce4e86db
Aaron Baldwin <baldwina@wwidea.org>
2017-04-10 13:13:48 -0600

    remove test stub

--- test/invalid_authenticity_token_rescue_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class InvalidAuthenticityTokenRescue::Test < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, InvalidAuthenticityTokenRescue
-  end
-end
