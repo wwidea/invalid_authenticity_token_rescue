448b39f0746470e9692f97d1f93845a75fb74c7a
Aaron Baldwin <baldwina@wwidea.org>
2017-04-10 13:08:21 -0600

    remove unused app argument

--- lib/invalid_authenticity_token_rescue/railtie.rb
+++ lib/invalid_authenticity_token_rescue/railtie.rb
@@ -1,6 +1,6 @@
 module InvalidAuthenticityTokenRescue
   class Railtie < Rails::Railtie
-    initializer "invalid_authenticity_token_rescue" do |app|
+    initializer "invalid_authenticity_token_rescue" do
       ActiveSupport.on_load :action_controller do
         ActionController::Base.include InvalidAuthenticityTokenRescue::RescueFromInvalidAuthenticityToken
       end
